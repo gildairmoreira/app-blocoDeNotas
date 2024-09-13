8d68b3c9c4f290c20045cc3cd95b10d837a96fb3
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-09-13 15:27:46 -0300

    feat: Adicionando o Salvamento de String Com sharedPreferences

--- app/src/main/java/com/gildair/blocodenotas/MainActivity.java
+++ app/src/main/java/com/gildair/blocodenotas/MainActivity.java
@@ -1,6 +1,10 @@
 package com.gildair.blocodenotas;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,16 +12,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
+        FloatingActionButton fbSalvar = findViewById(R.id.fb_salvar);
+        editAnotacao = findViewById(R.id.editAnotacao);
+
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+
+        fbSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoRecuperado = editAnotacao.getText().toString();
+
+                if (textoRecuperado.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Preencha a anotação", Toast.LENGTH_SHORT).show();
+                }else {
+                    preferencias.salvarAnotacao(textoRecuperado);
+                    Toast.makeText(getApplicationContext(), "Anotação salva com sucesso", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+        //Recuperar anotação
+        String anotacao = preferencias.recuperarAnotacao();
+        if (!anotacao.isEmpty()){
+            editAnotacao.setText(anotacao);
+        }
+
     }
 }
